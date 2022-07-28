3c663bbd8ee693948f8b2f906028a959297eb01a
Daniel dos Santos <daniel@automastersat.com.br>
2022-07-28 09:13:30 -0300

    CadastroDeProduto - Atualizado código e adicionado buscarPorId() e buscarTodos()

--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,6 +1,7 @@
 package br.com.alura.loja.testes;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
@@ -12,25 +13,28 @@ import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
 	public static void main(String[] args) {
-		Categoria celulares = new Categoria("CELULARES");
-
+		cadastrarProduto();
 		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		Produto produto = produtoDao.buscarPorId(1l);
+		System.out.println(produto.getPreco());
+		
+		List<Produto> todos = produtoDao.buscarTodos();
+		todos.forEach(p -> System.out.println(p.getNome()));
+	}
+
+	private static void cadastrarProduto() {
+		Categoria celulares = new Categoria("CELULARES");
+		Produto celular = new Produto("Xiaomi Redmi","Muito legal",
+		new BigDecimal("800"), celulares);
+		
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		CategoriaDao categoriaDao = new CategoriaDao(em);
 		em.getTransaction().begin();
-
-		em.persist(celulares); // -> MANAGED INSERT
-		celulares.setNome("XPTO"); // MANAGED UPDATE
-		em.flush(); // -> DETACHED
-		em.clear(); // -> DETACHED
-
-		celulares = em.merge(celulares); // -> MANAGED SELECT
-		celulares.setNome("1234"); // MANAGED UPDATE
-		em.flush(); // -> DETACHED
-
-//		em.getTransaction().commit(); // -> DETACHED
-		em.clear();
-		celulares = em.merge(celulares); // NECESSÁRIO ESTAR EM MANAGED PARA SER CHAMADO o remove() e flush()
-		em.remove(celulares); // -> REMOVED
-		em.flush();
+		categoriaDao.cadastrar(celulares);
+		produtoDao.cadastrar(celular);
+		em.getTransaction().commit();
 		em.close();
 	}
 }
